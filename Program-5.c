a5d2a2d87bd3481028f69483a5b8d329df2f3be0
KAMAL <20me0044@vvce.ac.in>
2021-03-02 13:26:24 +0000

    done

--- Program-5.c
+++ Program-5.c
@@ -8,14 +8,15 @@
  
 int main()
 {
-   int a[10],i,sum,n;
+   int a[10],i,sum=0,n;
    printf("Enter number of elements in array and the array elements");
    scanf("%d",&n);
-   printf("enter the elements of the array");
+
    for (i = 0; i < n; i++)
    {
       scanf("%d", &a[i]);
    }
+   printf("enter the elements of the array");
    for (i=0;i<n;i++)
    {
       sum=sum+a[i]; 
