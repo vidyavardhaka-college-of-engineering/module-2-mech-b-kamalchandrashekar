94255641b9f852d39c106900e4d2c83f23a4b90d
KAMAL <20me0044@vvce.ac.in>
2021-03-08 14:49:23 +0000

    done

--- Program-2.c
+++ Program-2.c
@@ -1,6 +1,6 @@
 //PRINTING FIRST N NATURAL NUMEBRS
 //INPUT -3
-//OUTPUT - 123
+//OUTPUT - 1 2 3
 
 
 #include<stdio.h>
@@ -11,6 +11,6 @@ void main()
 	scanf("%d",&n);
 	printf("The natural numbers are ");
 	for(i=1;i<=n;i++)
-		printf("%d",i);
+		printf("%d ",i);
 	//Fill the missing code here
  }
