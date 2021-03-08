2f32954b68641b676ca82284082c5aaa904afe07
KAMAL <20me0044@vvce.ac.in>
2021-03-08 14:44:11 +0000

    done

--- Program-10.c
+++ Program-10.c
@@ -33,7 +33,7 @@ int main()
         }
     }
 
-    printf("The largest and smallest element are  %d and %d", large,small);
+    printf("The largest and smallest elements are  %d and %d", large,small);
 
     return 0;
 }
